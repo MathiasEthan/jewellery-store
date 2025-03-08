50cd5dc0a01a2ed9ff6eb1108f80731fddbc7a84
HarshiiC <harshi.chaudharii@gmail.com>
2025-03-08 09:39:58 +0530

    added Header component and images

---
+++ components/Header.tsx
@@ -0,0 +1,4 @@
+<div className="w-full bg-[#091F42] py-4 text-center">
+      <img src="images/whitename&logo.png" alt="logo" className="mx-auto w-70 h-auto" />
+      <img src="/images/shopping-bag-white.png" alt="Shopping Bag" className="w-6 h-6 text-black" />
+      </div>
