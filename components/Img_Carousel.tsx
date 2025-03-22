82e8a661a97493cc83eb70e689e7f922b16f9dde
HarshiiC <harshi.chaudharii@gmail.com>
2025-03-22 09:11:47 +0530

    fixed Img_Carousel by importing react slick slider

--- components/Img_Carousel.tsx
+++ components/Img_Carousel.tsx
@@ -1,5 +1,9 @@
 'use client'
+//used to import react slick
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
+//images to be used in the carousel
 import Image from "next/image"; 
 import Img1 from "@/public/images/cimg1.jpg";
 import Img2 from "@/public/images/cimg2.jpg";
@@ -7,14 +11,25 @@ import Img3 from "@/public/images/cimg3.jpg";
 import Img4 from "@/public/images/cimg4.jpg";
 import Img5 from "@/public/images/cimg5.jpg";
 
+import React, { Component } from "react";
+import Slider from "react-slick";
 
-function Img_Carousel() {
-    return (
-      <>
-        <div className="carousel">
-            <div className="carousel__container w-full h-auto max-h-64 object-cover">
-                <div className="carousel_item">
-                <Image 
+function PauseOnHover() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true
+  };
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+      <div>
+        <h3>
+        <Image 
                 src="/images/cimg1.jpg" 
                 alt="img1" 
                 width={1200}   
@@ -22,9 +37,11 @@ function Img_Carousel() {
                 layout="responsive"
                 className="w-full h-auto max-h-130 object-cover"
                 />
-                </div>
-                <div className="carousel_item">
-                <Image 
+        </h3>
+      </div>
+      <div>
+        <h3>
+        <Image 
                 src={"/images/wideimg2.webp"} 
                 alt="img2"
                 width={1200}
@@ -32,23 +49,42 @@ function Img_Carousel() {
                 layout="responsive"
                 className="w-full h-auto max-h-130 object-cover"
                 />
-                </div>
-                <div className="carousel_item">
-                <Image 
+        </h3>
+      </div>
+      <div>
+        <h3>
+        <Image 
                 src={Img3} 
                 alt="img3"
+                width={1200}   
+                height={100}   
+                layout="responsive"
+                className="w-full h-auto max-h-130 object-cover"
                 />
-                </div>
-                <div className="carousel_item">
-                <Image 
+        </h3>
+      </div>
+      <div>
+        <h3>
+        <Image 
                 src={Img4} 
                 alt="img4"
+                width={1200}   
+                height={100}   
+                layout="responsive"
+                className="w-full h-auto max-h-130 object-cover"
                 />
-                </div>
-            </div>    
-        </div>
-      </>
-    );
-  }
-  
-  export default Img_Carousel;
+        </h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider>
+    <div>hi</div>
+    </div>
+  );
+}
+
+export default PauseOnHover;
