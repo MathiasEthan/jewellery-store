a33c45cc31b3db0c3162ac7d37784965a677974a
Ethan Mathias <ethanmathias123@gmail.com>
2025-03-22 06:15:03 +0000

    added cms

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-'use client'
+import { defineQuery } from "next-sanity";
+import { sanityFetch } from "./sanity/live";
 
 import Header from "@/components/Header";
 import Img_Carousel from "@/components/Img_Carousel";
 import Menu from "@/components/Menu";
 
-export default function Home() {
 
+export default async function Home() {
+  const {data: products} = await sanityFetch({query: EVENTS_QUERY});
   return (
     <>
       <Img_Carousel />
