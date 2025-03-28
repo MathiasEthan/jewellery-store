58809e37ca0827dcead7b75eda04415315944777
Ethan Mathias <ethanmathias123@gmail.com>
2025-03-28 08:09:17 +0000

    styling fixes

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,9 +14,9 @@ function Header() {
         <nav className="w-full p-8 pr-15 py-4 bg-gradient-to-r from-[#030D24] via-[#0A2A5A] to-[#133E7C] text-white shadow-lg">
         <div className="flex justify-between items-center w-full ">
             <Image src={Hamburg} alt="Menu" height={35} width={35} />
-
+            <Link href="/">
             <Image src={Logo} alt="Opulence Logo" height={300} width={300} />
-
+            </Link>
             <div className="flex space-x-4">
             <Image src={Wishlist} alt="Wishlist" height={20} width={20} />
             <Image src={User} alt="Account" height={20} width={20} />
